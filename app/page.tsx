517148a48f8ba9598cc6762f971e31d24edace3f
Joshua Hegedus <josh.hegedus@outlook.com>
2025-11-27 22:40:02 +0100

    feat: started using the header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FlashcardForm from "@/components/flashcard-form/FlashcardForm";
 import ThemeChanger from "@/components/theme-changer";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
+      <Header/>
       <FlashcardForm/>
-      <ThemeChanger />
+      <ThemeChanger/>
     </main>
   );
 }
